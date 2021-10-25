7acac4fc99a1011a44fb94de6afb3b1d6fec62a5
Lorenzo <lor.epifani@stud.uniroma3.it>
2021-10-25 17:04:19 +0200

    Reworked add-book modal page

--- books.js
+++ books.js
@@ -1,5 +1,12 @@
 const modal = document.querySelector(".modal-container");
-const addBtn = document.getElementById("add-btn");
+const addFloatBtn = document.getElementById("add-btn");
+
+/* Add book form */
+const addBookForm = document.getElementById("add-book-form");
+const bookTitle = document.getElementById("book-title");
+const bookAuthor = document.getElementById("book-author");
+const bookPages = document.getElementById("book-pages");
+
 
 let myLibrary = [];
 
@@ -21,14 +28,74 @@ function addBookToLibrary() {
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 256, true);
 
-console.log(theHobbit.info());
+function showErrorFor(input, errorMsg) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    small.innerText = errorMsg;
+    formControl.className = "form-control error";
+}
 
-addBtn.addEventListener("click", () => {
+function showSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success"
+}
+
+function checkForm() {
+    const titleInput = bookTitle.value;
+    const authorInput = bookAuthor.value;
+    const pagesInput = bookPages.value;
+    let validTitle = false;
+    let validAuthor = false;
+    let validPages = false;
+
+    if(titleInput === "") {
+        showErrorFor(bookTitle, "Please insert a book title");
+    }
+    else {
+        showSuccessFor(bookTitle);
+        validTitle = true;
+    }
+
+    if(authorInput === "") {
+        showErrorFor(bookAuthor, "Please insert an Author for your book");
+    }
+    else {
+        showSuccessFor(bookAuthor);
+        validAuthor = true;
+    }
+
+    if(pagesInput === "") {
+        showErrorFor(bookPages, "Please insert the total pages of your book");
+    }
+    else if(pagesInput < 0) {
+        showErrorFor(bookPages, "Please insert positive number for book pages");
+    }
+    else if(pagesInput == 0 || pagesInput > 99999) {
+        showErrorFor(bookPages, "Please select a number between 1 and 99999");
+    }
+    else {
+        showSuccessFor(bookPages);
+        validPages = true;
+    }
+
+    if(validTitle && validAuthor && validPages) {
+        console.log(titleInput);
+        console.log(authorInput);
+        console.log(pagesInput);
+    }
+}
+
+addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkForm();
+});
+
+addFloatBtn.addEventListener("click", () => {
     modal.style.display = "flex";
 });
 
-window.addEventListener("click", e => {
+window.addEventListener("click", (e) => {
     if(e.target === modal) {
         modal.style.display = "none";
     }
-})
+});
